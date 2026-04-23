5e6946c6323de4fd90ce8b2dd6fd316408751fe7
shaishab316 <shaishabchandrashil@gmail.com>
2026-04-23 16:04:22 +0600

    feat: add health check endpoint and integrate Terminus module in API gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './api-gateway.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
+    TerminusModule,
     ClientsModule.register([
       {
         name: 'USER_SERVICE',
@@ -20,6 +23,6 @@ import { AppController } from './api-gateway.controller';
       },
     ]),
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
 })
 export class AppModule {}
